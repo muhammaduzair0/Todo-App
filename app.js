e377d4b0f92fcd83c4308cab2aded50593110909
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-03-15 16:04:15 +0500

    Add DOM element selection for Todo List functionality

---
+++ app.js
@@ -0,0 +1,6 @@
+// DOM Elements
+
+let todoField = document.getElementById("todo-input");
+let todoList = document.getElementById("todo-list");
+let addBtn = document.getElementById("add-button");
+let clearAllBtn = document.getElementById("clear-all-button");
