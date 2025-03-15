fc65ba32aa95eb665284161ef96381e63732003a
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-03-15 17:08:53 +0500

    Add renderTodoData function to display todo items with delete and edit buttons

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // DOM Elements
 
 let todoField = document.getElementById("todo-input");
-let todoList = document.getElementById("todo-list");
+let todoListData = document.getElementById("todo-list");
 let addBtn = document.getElementById("add-button");
 let clearAllBtn = document.getElementById("clear-all-button");
+let todoList = JSON.parse(localStorage.getItem("todo-list")) || [];
+
+let renderTodoData = () => {
+  todoListData.innerHTML = "";
+  todoList.forEach((todo, index) => {
+    todoListData.innerHTML += `
+             <h1>${todo}</h1>
+             <Button onClick=deleteTodo(${index})>Delete</Button>
+             <Button onClick=editTodo(${index})>Edit</Button>
+             `;
+  });
+};
